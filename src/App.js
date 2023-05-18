5018443986017778a2247a8198ad888bf067fe61
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-19 01:52:21 +0600

    adding signup form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
+import React from "react";
+// import Home from "./routes/SignupForm";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error.page.jsx";
+import SignupForm from "./routes/SignupForm";
+// import CpGuide from "./routes/CP-Guide";
+// import SigninForm from "./routes/SigninForm";
+// import SignupForm from "./routes/SignupForm";
+// import TermsPrivacy from "./routes/Terms-Privacy";
+// import ContactUs from "./routes/ContactUs";
+// import Challenges from "./routes/Challenges";
+// import Quiz from "./routes/Quiz";
+// import AboutUs from "./routes/AboutUs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignupForm />,
+    errorElement: <ErrorPage />,
+  },
+  // {
+  //   path: "/signin",
+  //   element: <SigninForm />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/signup",
+  //   element: <SignupForm />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/cpguide",
+  //   element: <CpGuide />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/contact-us",
+  //   element: <ContactUs />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/challenge",
+  //   element: <Challenges />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/quiz",
+  //   element: <Quiz />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/about-us",
+  //   element: <AboutUs />,
+  //   errorElement: <ErrorPage />,
+  // },
+  // {
+  //   path: "/term&privacy",
+  //   element: <TermsPrivacy />,
+  //   errorElement: <ErrorPage />,
+  // },
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Teacher Go</header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
