1733393592bf3b8d3cdc541d455580a16c2f608c
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-19 16:46:23 +0600

    adding sign in form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
-// import Home from "./routes/SignupForm";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error.page.jsx";
 
+import SignInForm from "./routes/SignInForm.jsx";
 import TeacherSignupForm from "./routes/TeacherSignupForm";
 import DriverSignupForm from "./routes/DriverSignupForm.jsx";
-// import CpGuide from "./routes/CP-Guide";
-// import SigninForm from "./routes/SigninForm";
-// import SignupForm from "./routes/SignupForm";
-// import TermsPrivacy from "./routes/Terms-Privacy";
-// import ContactUs from "./routes/ContactUs";
-// import Challenges from "./routes/Challenges";
-// import Quiz from "./routes/Quiz";
-// import AboutUs from "./routes/AboutUs";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <SignInForm />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/teacher-signup",
     element: <TeacherSignupForm />,
     errorElement: <ErrorPage />,
   },
@@ -25,46 +22,6 @@ const router = createBrowserRouter([
     element: <DriverSignupForm />,
     errorElement: <ErrorPage />,
   },
-  // {
-  //   path: "/signin",
-  //   element: <SigninForm />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/signup",
-  //   element: <SignupForm />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/cpguide",
-  //   element: <CpGuide />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/contact-us",
-  //   element: <ContactUs />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/challenge",
-  //   element: <Challenges />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/quiz",
-  //   element: <Quiz />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/about-us",
-  //   element: <AboutUs />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/term&privacy",
-  //   element: <TermsPrivacy />,
-  //   errorElement: <ErrorPage />,
-  // },
 ]);
 
 function App() {
---
+++ src/routes/SignInForm.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import "../styles/signin.css";
+import { useFormik } from "formik";
+import { signInSchema } from "../schemas/schemas";
+import { Link } from "react-router-dom";
+
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+const onSubmit = async (values, actions) => {
+  console.log(values);
+  console.log(actions);
+  console.log("ok");
+  console.log(JSON.stringify(values));
+
+  //   let result = await fetch("http://localhost:4000/api/users/signin", {
+  //     method: "POST",
+  //     body: JSON.stringify(values),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   result = await result.json();
+  //   // console.log("result--> ", result.newUser);
+  //   actions.resetForm();
+
+  //   if (result.newUser) {
+  //     toast.success("Account has been created !", {
+  //       position: toast.POSITION.TOP_RIGHT,
+  //     });
+  //     setTimeout(() => {
+  //       window.location.href = "/signin";
+  //     }, 2000);
+  //   } else {
+  //     toast.warning("Email is already used try another email !", {
+  //       position: toast.POSITION.TOP_RIGHT,
+  //     });
+  //   }
+};
+
+const SignInForm = () => {
+  const [choose, setChoose] = useState(false);
+  const handleChooseClick = (param) => {
+    setChoose(param);
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // scroll to the top of the page
+  }, []);
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: signInSchema,
+    onSubmit,
+  });
+
+  console.log(errors);
+  return (
+    <>
+      {!choose ? (
+        <main class="sign-main">
+          <div class="sign-container">
+            <div class="sign-form-container">
+              <h1>Login into Teacher-Go</h1>
+              <form
+                class="sign-login sign-form"
+                onSubmit={handleSubmit}
+                autoComplete="off"
+              >
+                {/* email */}
+                <label htmlFor="email">Email</label>
+                <input
+                  value={values.email}
+                  onChange={handleChange}
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  onBlur={handleBlur}
+                  className={errors.email && touched.email ? "input-error" : ""}
+                />
+                {errors.email && touched.email && (
+                  <p className="error">{errors.email}</p>
+                )}
+
+                {/* passwword */}
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "input-error" : ""
+                  }
+                />
+                {errors.password && touched.password && (
+                  <p className="error">{errors.password}</p>
+                )}
+
+                {/* button */}
+                <button disabled={isSubmitting} type="submit" class="button">
+                  Submit
+                </button>
+                {/* <ToastContainer /> */}
+
+                {/* login  */}
+                <div
+                  className="sign-info"
+                  onClick={() => {
+                    handleChooseClick(true);
+                  }}
+                >
+                  Don't have an account? <Link className="link">Signup</Link>{" "}
+                  instead.
+                </div>
+              </form>
+            </div>
+          </div>
+        </main>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default SignInForm;
