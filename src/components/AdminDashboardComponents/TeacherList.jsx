6ce7918394a594d1966722092b0051ff27531252
Md. Imran Mir <imranmir6677@gmail.com>
2023-05-21 01:49:07 +0600

    adding image in teacherlist

--- src/components/AdminDashboardComponents/TeacherList.jsx
+++ src/components/AdminDashboardComponents/TeacherList.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "../../styles/Dashboard/components.css";
 import imran from "../../assets/imran.jpg";
+import olin from "../../assets/olin.jpg";
+import rafi from "../../assets/rafi.jpg";
+import likhon from "../../assets/likhon.jpg";
 
 // import { ToastContainer, toast } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
@@ -24,30 +27,33 @@ const TeacherList = () => {
           mail: "imran@gmail.com",
         },
       ],
+      image: imran,
     },
     {
       id: 2,
       details: [
         {
-          name: "Rafi Hasan",
+          name: "Olin Akon Chumky",
           teacher_id: 1944651011,
           phone_no: "01911111111",
           route: "Campus-Mohakhali",
           mail: "rafi@gmail.com",
         },
       ],
+      image: olin,
     },
     {
       id: 3,
       details: [
         {
-          name: "Kamran Mir",
+          name: "Tanvir Hasan",
           teacher_id: 1944651069,
           phone_no: "01911111111",
           route: "Campus-Mohakhali",
           mail: "kamran@gmail.com",
         },
       ],
+      image: rafi,
     },
     {
       id: 4,
@@ -60,6 +66,7 @@ const TeacherList = () => {
           mail: "1944651079@uits.edu.bd",
         },
       ],
+      image: likhon,
     },
     {
       id: 1,
@@ -72,18 +79,20 @@ const TeacherList = () => {
           mail: "imran@gmail.com",
         },
       ],
+      image: imran,
     },
     {
       id: 2,
       details: [
         {
-          name: "Rafi Hasan",
+          name: "Olin Akon Chumky",
           teacher_id: 1944651011,
           phone_no: "01911111111",
           route: "Campus-Mohakhali",
           mail: "rafi@gmail.com",
         },
       ],
+      image: olin,
     },
     {
       id: 3,
@@ -93,9 +102,10 @@ const TeacherList = () => {
           teacher_id: 1944651069,
           phone_no: "01911111111",
           route: "Campus-Mohakhali",
-          mail: "1944651069@uits.edu.bd",
+          mail: "kamran@gmail.com",
         },
       ],
+      image: rafi,
     },
     {
       id: 4,
@@ -108,54 +118,7 @@ const TeacherList = () => {
           mail: "1944651079@uits.edu.bd",
         },
       ],
-    },
-    {
-      id: 1,
-      details: [
-        {
-          name: "Md. Imran Mir",
-          teacher_id: 1944651035,
-          phone_no: "01911111111",
-          route: "Campus-Rampura",
-          mail: "imran@gmail.com",
-        },
-      ],
-    },
-    {
-      id: 2,
-      details: [
-        {
-          name: "Rafi Hasan",
-          teacher_id: 1944651011,
-          phone_no: "01911111111",
-          route: "Campus-Mohakhali",
-          mail: "rafi@gmail.com",
-        },
-      ],
-    },
-    {
-      id: 3,
-      details: [
-        {
-          name: "Tanvir Hasan",
-          teacher_id: 1944651069,
-          phone_no: "01911111111",
-          route: "Campus-Mohakhali",
-          mail: "1944651069@uits.edu.bd",
-        },
-      ],
-    },
-    {
-      id: 4,
-      details: [
-        {
-          name: "Murtoza Likhon",
-          teacher_id: 1944651079,
-          phone_no: "01911111111",
-          route: "Campus-Mohakhali",
-          mail: "1944651079@uits.edu.bd",
-        },
-      ],
+      image: likhon,
     },
   ];
   return (
@@ -164,7 +127,8 @@ const TeacherList = () => {
         {demoTeacherList.map((demoTeacherList) => (
           <div className="list-details">
             <div className="request-all-details">
-              <img src={imran} alt="imran" />
+              {/* <p>{demoTeacherList.image}</p> */}
+              <img src={demoTeacherList.image} alt="imran" />
             </div>
             <div className="request-all-details">
               <div key={demoTeacherList.id}>
